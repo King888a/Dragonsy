17eb6851008d75a9a4c32a06f8d96c7e80b66f11
dragoiq <119659334+qithoniq@users.noreply.github.com>
2023-04-23 02:23:45 +0300

    Update helpers.py

--- sbb_b/core/helpers.py
+++ sbb_b/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("Drago")
+LOGGER = logging.getLogger("سورس دراكو")
 
 
 def printUser(entity: types.User) -> None:
