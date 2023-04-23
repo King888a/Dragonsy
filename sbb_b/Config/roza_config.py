2a49321d47e28b51d35d5b7ca4879d608e62a232
dragoiq <119659334+qithoniq@users.noreply.github.com>
2023-04-23 06:06:37 +0300

    Update roza_config.py

--- sbb_b/Config/roza_config.py
+++ sbb_b/Config/roza_config.py
@@ -7,4 +7,4 @@ ENV = bool(os.environ.get("ENV", False))
 if ENV:
     from sample_config import Config  # noqa
 elif os.path.exists("config.py"):
-    from config import Development as Config  # noqa
+    from config import Development e Config  # noqa
